d504170c0bcdca047a0f7b1a5d41dc1e05aa9bd4
András Veres-Szentkirályi <vsza@vsza.hu>
2010-07-05 23:10:24 +0200

    Created Erlang interface to query HackSense

---
+++ hacksense.erl
@@ -0,0 +1,38 @@
+% Copyright (c) 2010 András Veres-Szentkirályi
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
+
+-module(hacksense).
+-compile(export_all).
+
+-include("hacksense.hrl").
+
+state() -> state("http://vsza.hu/hacksense").
+state(BaseUrl) ->
+	{ok, {_, _, CSV}} = httpc:request(BaseUrl ++ "/status.csv"),
+	[GUID, Timestamp, State | _] = string:tokens(
+		string:strip(CSV, right, $\n), ";"),
+	#hackstate{
+		guid = GUID, timestamp = Timestamp, state = list_to_integer(State)}.
+
+state_to_list(#hackstate{timestamp = Timestamp, state = State}) ->
+	case State of
+		0 -> "CLOSED";
+		_ -> "OPEN"
+	end ++ " since " ++ Timestamp.
