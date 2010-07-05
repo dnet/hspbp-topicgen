f41a904c99e4453c95ab9e69006fcc257480be54
András Veres-Szentkirályi <vsza@vsza.hu>
2010-07-06 00:03:13 +0200

    Added entry point for dnet's fork of erlang-ircbot
    Using this topic generator, the state of HackSense can now be linked to
    an instance of dnet's fork of erlang-ircbot, which is also available
    at GitHub: http://github.com/dnet/erlang-ircbot

---
+++ topicgen.erl
@@ -0,0 +1,44 @@
+% Copyright (c) 2010 András Veres-Szentkirályi
+%
+% Permission is hereby granted, free of charge, to any person obtaining a copy
+% of this software and associated documentation files (the "Software"), to deal
+% in the Software without restriction, including without limitation the rights
+% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+% copies of the Software, and to permit persons to whom the Software is
+% furnished to do so, subject to the following conditions:
+%
+% The above copyright notice and this permission notice shall be included in
+% all copies or substantial portions of the Software.
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+% THE SOFTWARE.
+
+-module(topicgen).
+-compile(export_all).
+
+% check HackSense every Interval ms, change topic if necessary, quit @ quit msg
+ircproc(Interval, Pid, HSURL, HSState) ->
+	ircproc(Interval, Pid, HSURL, HSState, false).
+ircproc(Interval, Pid, HSURL, HSState, Update) ->
+	hacksense:send_if_newer(HSURL, HSState),
+	case Update of
+		true -> Pid ! {topic,
+			"http://hspbp.org || " ++ hacksense:state_to_list(HSState)};
+		false -> ok
+	end,
+	receive
+		{hacksense, NewHSState} ->
+			ircproc(Interval, Pid, HSURL, NewHSState, true);
+		quit -> quit
+		after Interval -> ircproc(Interval, Pid, HSURL, HSState)
+	end.
+
+% entry point for dnet's fork of erlang-ircbot
+ircmain(Bot, HSURL) -> ircmain(Bot, HSURL, 90000).
+ircmain(Bot, HSURL, Interval) ->
+	spawn(?MODULE, ircproc, [Interval, Bot, HSURL, invalid]).
